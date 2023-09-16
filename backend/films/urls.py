8dbeee40fe58264180534498d9fcaee6c0e2497d
Alan <a_saudabekov@kbtu.kz>
2023-09-16 20:25:46 +0600

    viewset and simple router

--- backend/films/urls.py
+++ backend/films/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 from . import views
 
+router = SimpleRouter()
+router.register(r'', views.FilmViewSet)
+
 urlpatterns = [
-    path('', views.FilmListAPIView.as_view()),
-    path('<int:pk>/', views.FilmDetailAPIView.as_view()),
+    # path('', views.FilmListAPIView.as_view()),
+    # path('<int:pk>/', views.FilmDetailAPIView.as_view()),
+    # path('', views.FilmViewSet.as_view({'get': 'list'})),
+    # path('<int:pk>/', views.FilmViewSet.as_view({'get': 'retrieve'})),
+    path('', include(router.urls)),
 ]
--- backend/films/views.py
+++ backend/films/views.py
@@ -2,78 +2,96 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 from .models import *
 from .serializers import *
 
 
 # Create your views here.
-class FilmListAPIView(ListCreateAPIView):
+
+
+
+class FilmViewSet(ModelViewSet):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
 
+    @action(methods=['get'], detail=False)
+    def genres(self, request):
+        genres = Genre.objects.all()
+        return Response([genre.name for genre in genres]) # list comprehension
 
-class FilmDetailAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Film.objects.all()
-    serializer_class = FilmSerializer
+    @action(methods=['get'], detail=False)
+    def genre_filter(self, request):
+        films = Film.objects.filter(genre_id=self.request.query_params.get('genre_id'))
+        serializer = FilmSerializer(films, many=True)
+        return Response(serializer.data)
+
+
+## localhost:8000/api/films/genre_filter?genre_id=1
 
 
 
-class FilmAPIView(APIView):
-    def get(self, request):
-        films = Film.objects.all()
-        return Response({'film': FilmSerializer(films, many=True).data})
+
+
+
+
+# class FilmAPIView(APIView):
+#     def get(self, request):
+#         films = Film.objects.all()
+#         return Response({'film': FilmSerializer(films, many=True).data})
     
-    def post(self, request):
-        # new_film = Film.objects.create(
-        #     poster=request.data['poster'],
-        #     title_ru=request.data['title_ru'],
-        #     title_orig=request.data['title_orig'],
-        #     prod_year=request.data['prod_year'],
-        #     timing=request.data['timing'],
-        #     premiere_date=request.data['premiere_date'],
-        #     country_id=request.data['country_id'],
-        #     genre_id=request.data['genre_id'],
-        #     director_id=request.data['director_id'],
-        # )
-        # new_film.save()
+#     def post(self, request):
+#         # new_film = Film.objects.create(
+#         #     poster=request.data['poster'],
+#         #     title_ru=request.data['title_ru'],
+#         #     title_orig=request.data['title_orig'],
+#         #     prod_year=request.data['prod_year'],
+#         #     timing=request.data['timing'],
+#         #     premiere_date=request.data['premiere_date'],
+#         #     country_id=request.data['country_id'],
+#         #     genre_id=request.data['genre_id'],
+#         #     director_id=request.data['director_id'],
+#         # )
+#         # new_film.save()
 
-        # return Response({'film': FilmSerializer(new_film).data})
+#         # return Response({'film': FilmSerializer(new_film).data})
 
-        serializer = FilmSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+#         serializer = FilmSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
 
-        return Response({'film': serializer.data})
-    
-
-    def put(self, request, *args, **kwargs):        # Обновить данные 
-        pk = kwargs.get('pk', None)
-
-        if not pk:
-            return Response({'error': 'Method "PUT" not allowed.'})
-        
-        try:
-            instance = Film.objects.get(pk=pk)
-        except:
-            return Response({'error': 'Object does not exists.'})
-        
-        serializer = FilmSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response({'film': serializer.data})
+#         return Response({'film': serializer.data})
     
 
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
+#     def put(self, request, *args, **kwargs):        # Обновить данные 
+#         pk = kwargs.get('pk', None)
 
-        if not pk:
-            return Response({'error': 'Method "DELETE" not allowed.'})
+#         if not pk:
+#             return Response({'error': 'Method "PUT" not allowed.'})
         
-        try:
-            instance = Film.objects.get(pk=pk)
-            instance.delete()
-        except:
-            return Response({'error': 'Object does not exists.'})
+#         try:
+#             instance = Film.objects.get(pk=pk)
+#         except:
+#             return Response({'error': 'Object does not exists.'})
         
-        return Response({'status': 'Film was deleted.'})
+#         serializer = FilmSerializer(data=request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+
+#         return Response({'film': serializer.data})
+    
+
+#     def delete(self, request, *args, **kwargs):
+#         pk = kwargs.get('pk', None)
+
+#         if not pk:
+#             return Response({'error': 'Method "DELETE" not allowed.'})
+        
+#         try:
+#             instance = Film.objects.get(pk=pk)
+#             instance.delete()
+#         except:
+#             return Response({'error': 'Object does not exists.'})
+        
+#         return Response({'status': 'Film was deleted.'})
